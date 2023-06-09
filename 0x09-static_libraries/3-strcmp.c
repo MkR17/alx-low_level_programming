7e23fe3cf7c47b7ed251d5cefad2f3cdae58563f
Mkateko <mkatekorikhotso.mr@gmail.com>
2023-06-09 00:56:43 -0700

    creating libraries

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcmp -a function that compares string values
+ * @s1: input number
+ * @s2: input number
+ *
+ * Return: s1[i] - s2[i]
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	i++;
+	}
+	return (0);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs -a function thst computes the absolute value of an integer
+ * @n: the int to be checked
+ * Return: the absolute value of integery
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
